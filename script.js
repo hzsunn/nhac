8008a960392b9602ec168c23f5b630013a0c04a3
Đặng Hồng <48261223+hzsunn@users.noreply.github.com>
2025-11-12 16:01:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const commonThumbnail = "huyen.JPG";
  
 const tracks = [
   { name: "Em Ơi Em Đừng Khóc", file: "music/Emoiem.m4a" },
+ { name: "Có Mình Và Ta", file: "music/Cominhvata.m4a" },
   { name: "Thương Em", file: "music/Thuongem.m4a" },
  { name: "Anh Thua Anh Ta", file: "music/Anhthua.m4a" },
   { name: "Ngày Này Năm Ấy", file: "music/Nnnay.m4a" },
